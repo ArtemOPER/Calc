01e52c8fb14938650453630d362b71ba51692fea
Glan Artem <artemglan@mail.ru>
2021-02-03 18:59:27 +0400

    Добавьте файлы проекта.

---
+++ калькулятор/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace калькулятор
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                double firstValue, secondValue;
+                string action;
+
+                Console.Clear();
+
+                Console.WriteLine("Введите число 1");
+                firstValue = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Введите число 2");
+                secondValue = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Выбирите операцию: '/' '*' '-' '+' ");
+                action = Console.ReadLine();
+
+                if (action == "+")
+                {
+                    Console.WriteLine(firstValue + secondValue);
+                }
+                else if (action == "-")
+                {
+                    Console.WriteLine(firstValue - secondValue);
+                }
+                else if (action == "*")
+                {
+                    Console.WriteLine(firstValue * secondValue);
+                }
+                else if (action == "/")
+                {
+                    if (secondValue == 0)
+                        Console.WriteLine(0);
+                    else
+                        Console.WriteLine(firstValue / secondValue);
+                }
+                else
+                {
+                    Console.WriteLine("Неправильная операция");
+                }
+
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
---
+++ калькулятор2/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace калькулятор2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                double firstValue, secondValue;
+                string action;
+
+                Console.Clear();
+
+                Console.WriteLine("Введите число 1");
+                firstValue = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Введите число 2");
+                secondValue = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Выбирите операцию: '/' '*' '-' '+' ");
+                action = Console.ReadLine();
+
+                switch (action)
+                {
+                    case "+":
+                        Console.WriteLine(firstValue + secondValue);
+                        break;
+
+                    case "-":
+                        Console.WriteLine(firstValue - secondValue);
+                        break;
+
+                    case "*":
+                        Console.WriteLine(firstValue * secondValue);
+                        break;
+
+                    case "/":
+                        if (secondValue == 0)
+                            Console.WriteLine(0);
+                        Console.WriteLine(firstValue / secondValue);
+                        break;
+
+                    default:
+                        Console.WriteLine("Ошибка, Неизвестная операция");
+                        break;
+
+
+
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+}
